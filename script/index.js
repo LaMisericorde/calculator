6cab65d4d5c2db5f83c87aff8b1d73d548932b42
Friedrich Erbach <friedrich.erbach@protonmail.com>
2024-01-02 19:49:31 +0200

    Add the period functionality

--- script/index.js
+++ script/index.js
@@ -28,9 +28,12 @@ const secondOperandDisplay = document.querySelector(".second-operand");
 const operatorDisplay = document.querySelector(".operator-display");
 
 const numberButtons = document.querySelectorAll(".number");
+const periodButton = document.querySelector(".period");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalButton = document.querySelector(".equal");
+const signButton = document.querySelector(".sign");
 const clearButton = document.querySelector(".clear");
+
 
 // Number Buttons
 
@@ -39,6 +42,27 @@ numberButtons.forEach((button) => {
         addNumberToDisplay(button);       
     });
 });
+
+periodButton.addEventListener("click", () => {
+    if (operandNumber == 1) {
+        if (checkIfNumberStartsWithPeriod()) {
+            if (firstOperandDisplay.textContent != "") {
+                if (!(firstOperandDisplay.textContent.indexOf(".") >= 0)) {
+                    firstOperandDisplay.textContent = firstOperandDisplay.textContent + periodButton.textContent;
+                }
+            }
+        }
+    } else if (operandNumber == 2) {
+        if (checkIfNumberStartsWithPeriod()) {
+            if (secondOperandDisplay.textContent != "") {
+                if (!(secondOperandDisplay.textContent.indexOf(".") >= 0)) {
+                    secondOperandDisplay.textContent = secondOperandDisplay.textContent + periodButton.textContent;
+                }
+            }
+        }
+    }
+});
+
 
 // Based on the format of the last operation display we search for a space to see if it has the form (operand operator operand) or not;
 
@@ -60,6 +84,7 @@ function addNumberToDisplay(button) {
 }
 
 
+
 function checkIfNumberStartsWithZero(button) {
     if (operandNumber == 1) {
         if (firstOperandDisplay.textContent == "0" && (button.textContent == "0")) {
@@ -69,6 +94,22 @@ function checkIfNumberStartsWithZero(button) {
         }
     } else if (operandNumber == 2) {
         if (secondOperandDisplay.textContent == "0" && (button.textContent == "0")) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
+function checkIfNumberStartsWithPeriod() {
+    if (operandNumber == 1) {
+        if (firstOperandDisplay.textContent == ".") {
+            return 0;
+        } else {
+            return 1;
+        }
+    } else if (operandNumber == 2) {
+        if (secondOperandDisplay.textContent == ".") {
             return 0;
         } else {
             return 1;
@@ -158,6 +199,8 @@ clearButton.addEventListener("click", () => {
     firstOperandDisplay.textContent = "";
     lastOperationDisplay.textContent = "";
 });
+
+// Sign Button
 
 
 
